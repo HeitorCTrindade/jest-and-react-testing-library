572351c4e19c5931dfcf92fb76626415b107d1aa
HeitorCTrindade <heitor-trindade@hotmail.com>
2022-08-16 19:04:18 -0300

    Requisito 3 - finalizado e matando todos os mutantes.

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,77 @@
-test('', () => {});
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import { FavoritePokemons } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+const fakeFavoritesPokemons = [
+  {
+    id: 25,
+    name: 'Pikachu',
+    type: 'Electric',
+    averageWeight: {
+      value: '6.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Kanto Viridian Forest',
+        map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+      },
+      {
+        location: 'Kanto Power Plant',
+        map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+      },
+    ],
+    summary: 'This intelligent Pokémon roasts hard berries with electricity to make them tender enough to eat.',
+  },
+  {
+    id: 4,
+    name: 'Charmander',
+    type: 'Fire',
+    averageWeight: {
+      value: '8.5',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/0/0a/Spr_5b_004.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Alola Route 3',
+        map: 'https://cdn2.bulbagarden.net/upload/9/93/Alola_Route_3_Map.png',
+      },
+      {
+        location: 'Kanto Route 3',
+        map: 'https://cdn2.bulbagarden.net/upload/4/4a/Kanto_Route_3_Map.png',
+      },
+      {
+        location: 'Kanto Route 4',
+        map: 'https://cdn2.bulbagarden.net/upload/2/24/Kanto_Route_4_Map.png',
+      },
+      {
+        location: 'Kanto Rock Tunnel',
+        map: 'https://cdn2.bulbagarden.net/upload/6/6f/Kanto_Rock_Tunnel_Map.png',
+      },
+    ],
+    summary: 'The flame on its tail shows the strength of its life force. If it is weak, the flame also burns weakly.',
+  },
+];
+
+describe('test FavoritePokemons Component operation', () => {
+  test('if no exist favorites Pokémons, show correct mensage', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const notFoundMessage = screen.getByText('No favorite pokemon found');
+    const pokemonsCards = screen.queryAllByTestId('pokemon-name');
+    expect(notFoundMessage).toBeInTheDocument();
+    expect(pokemonsCards.length).toBe(0);
+  });
+
+  test('if exist favorites Pokémons, render Cards correctly', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ fakeFavoritesPokemons } />);
+    const notFoundMessage = screen.queryByText('No favorite pokemon found');
+    const pokemonsCards = screen.queryAllByTestId('pokemon-name');
+    expect(notFoundMessage).toBe(null);
+    expect(pokemonsCards.length).toBe(2);
+  });
+});
